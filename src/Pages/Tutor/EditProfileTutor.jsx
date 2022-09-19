de9082e1442800befd8ef58471e5c90d387c64b4
tangjeraldine <typjera.1127@gmail.com>
2022-09-19 16:01:52 +0800

    Pulling changes.

--- src/Pages/Tutor/EditProfileTutor.jsx
+++ src/Pages/Tutor/EditProfileTutor.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useEffect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import signUpAsTutorValidation from "../../Validations/signUpAsTutorValidation";
 import { Field, Formik, Form, useFormikContext } from "formik";
 import { useEffect, useState } from "react";
@@ -120,216 +120,228 @@ const EditTutorProfile = () => {
     setWantToEdit(true);
   };
 
-  return (
-    <>
-      {wantToEdit}
-      <p>Full Name: </p>
-      <button style={{ backgroundColor: "lime" }} onClick={handleEdit}>
-        Edit Profile
-      </button>
-      <br />
+  const handleCompleteEditing = () => {
+    setWantToEdit(false);
+  };
 
-      <h1 style={{ fontSize: "50px" }}>View Tutor Profile</h1>
+  if (!wantToEdit) {
+    return (
+      <div>
+        <p>Full Name: </p>
+        <button style={{ backgroundColor: "lime" }} onClick={handleEdit}>
+          Edit Profile
+        </button>
+        <br />
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <h1 style={{ fontSize: "50px" }}>View Tutor Profile</h1>
 
-      {/* using formik */}
-      <Formik
-        initialValues={{
-          fullName: "",
-          phone: "",
-          region: "select",
-          rates: "",
-          classType: "select",
-          classLevel: [],
-          subjects: [],
-          educationBackground: "",
-          teachingExperience: "",
-        }}
-        validationSchema={signUpAsTutorValidation}
-        onSubmit={(values) => {
-          console.log(values);
-          handleSignUpAsTutor(values);
-        }}>
-        {({
-          handleChange,
-          handleBlur,
-          values,
-          errors,
-          touched,
-          initialValues,
-        }) => (
-          <Form>
-            <p>Full Name</p>
-            <Field
-              name='fullName'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.fullName}
-            />
-            {errors.fullName && touched.fullName ? (
-              <div>{errors.fullName}</div>
-            ) : null}
-            <br />
-
-            <p>Phone</p>
-            <Field
-              name='phone'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.phone}
-            />
-            {errors.phone && touched.phone ? <div>{errors.phone}</div> : null}
-            <br />
-
-            <p>Region</p>
-            <Field
-              as='select'
-              name='region'
-              values={values.region}
-              onChange={handleChange}>
-              <option disabled>select</option>
-              <option value='North'>North</option>
-              <option value='South'>South</option>
-              <option value='East'>East</option>
-              <option value='West'>West</option>
-              <option value='Central'>Central</option>
-            </Field>
-            {errors.region && touched.region ? (
-              <div>{errors.region}</div>
-            ) : null}
-            <br />
-            <br />
-
-            <p>Rates per lesson</p>
-            <Field
-              name='rates'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.rates}
-            />
-            {errors.rates && touched.rates ? <div>{errors.rates}</div> : null}
-            <br />
-
-            <p>Class Type</p>
-            <Field
-              as='select'
-              name='classType'
-              values={values.classType}
-              onChange={handleChange}>
-              <option disabled>select</option>
-              <option value='In-Person'>In-Person</option>
-              <option value='Remote'>Remote</option>
-              <option value='Both In-Person and Remote'>Both</option>
-            </Field>
-            {errors.classType && touched.classType ? (
-              <div>{errors.classType}</div>
-            ) : null}
-            <br />
-            <br />
-
-            <p>Class Level</p>
-            <div>
-              Primary
-              {priClassLevel.map((level) => {
-                return (
-                  <div key={level}>
-                    <Field type='checkbox' name='classLevel' value={level} />
-                    {level}
-                  </div>
-                );
-              })}
+        {/* using formik */}
+        <Formik
+          initialValues={{
+            fullName: "",
+            phone: "",
+            region: "select",
+            rates: "",
+            classType: "select",
+            classLevel: [],
+            subjects: [],
+            educationBackground: "",
+            teachingExperience: "",
+          }}
+          validationSchema={signUpAsTutorValidation}
+          onSubmit={(values) => {
+            console.log(values);
+            handleSignUpAsTutor(values);
+          }}>
+          {({
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched,
+            initialValues,
+          }) => (
+            <Form>
+              <p>Full Name</p>
+              <Field
+                name='fullName'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.fullName}
+              />
+              {errors.fullName && touched.fullName ? (
+                <div>{errors.fullName}</div>
+              ) : null}
               <br />
-              Secondary
-              {secClassLevel.map((level) => {
-                return (
-                  <div key={level}>
-                    <Field type='checkbox' name='classLevel' value={level} />
-                    {level}
-                  </div>
-                );
-              })}
-            </div>
-            {errors.classLevel && touched.classLevel ? (
-              <div>{errors.classLevel}</div>
-            ) : null}
-            <br />
 
-            <p>Subjects</p>
-            <div>
-              Primary
-              {priSubjects.map((subject) => {
-                return (
-                  <div key={subject}>
-                    <Field type='checkbox' name='subjects' value={subject} />
-                    {subject}
-                  </div>
-                );
-              })}
+              <p>Phone</p>
+              <Field
+                name='phone'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.phone}
+              />
+              {errors.phone && touched.phone ? <div>{errors.phone}</div> : null}
               <br />
-              Secondary
-              {secSubjects.map((subject) => {
-                return (
-                  <div key={subject}>
-                    <Field type='checkbox' name='subjects' value={subject} />
-                    {subject}
-                  </div>
-                );
-              })}
-              <br />
-              Primary/Secondary
-              <br />
-              <Field type='checkbox' name='subjects' value='English' />
-              English
-            </div>
-            {errors.subjects && touched.subjects ? (
-              <div>{errors.subjects}</div>
-            ) : null}
-            {!matchingLevelSub && (
-              <p>Please select matching class levels and subjects.</p>
-            )}
-            <br />
-            <br />
 
-            <p>Education Background</p>
-            <Field
-              name='educationBackground'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.educationBackground}
-            />
-            {errors.educationBackground && touched.educationBackground ? (
-              <div>{errors.educationBackground}</div>
-            ) : null}
-            <p>Teaching Experience</p>
-            <Field
-              name='teachingExperience'
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.teachingExperience}
-            />
-            {errors.teachingExperience && touched.teachingExperience ? (
-              <div>{errors.teachingExperience}</div>
-            ) : null}
-            <br />
-            <button
-              type='submit'
-              disabled={
-                !(
-                  Object.keys(errors).length === 0 &&
-                  Object.keys(touched).length ===
-                    Object.keys(initialValues).length
-                )
-              }
-              style={{ backgroundColor: "lime" }}>
-              sign up
-            </button>
-            {/* {!isEmailUnique && <p>Email already in use!</p>} */}
-            {!isTutorProfileSetUp && <p>Tutor profile unable to be set up.</p>}
-            <CheckClassLevelAndSubject />
-          </Form>
-        )}
-      </Formik>
-    </>
-  );
+              <p>Region</p>
+              <Field
+                as='select'
+                name='region'
+                values={values.region}
+                onChange={handleChange}>
+                <option disabled>select</option>
+                <option value='North'>North</option>
+                <option value='South'>South</option>
+                <option value='East'>East</option>
+                <option value='West'>West</option>
+                <option value='Central'>Central</option>
+              </Field>
+              {errors.region && touched.region ? (
+                <div>{errors.region}</div>
+              ) : null}
+              <br />
+              <br />
+
+              <p>Rates per lesson</p>
+              <Field
+                name='rates'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.rates}
+              />
+              {errors.rates && touched.rates ? <div>{errors.rates}</div> : null}
+              <br />
+
+              <p>Class Type</p>
+              <Field
+                as='select'
+                name='classType'
+                values={values.classType}
+                onChange={handleChange}>
+                <option disabled>select</option>
+                <option value='In-Person'>In-Person</option>
+                <option value='Remote'>Remote</option>
+                <option value='Both In-Person and Remote'>Both</option>
+              </Field>
+              {errors.classType && touched.classType ? (
+                <div>{errors.classType}</div>
+              ) : null}
+              <br />
+              <br />
+
+              <p>Class Level</p>
+              <div>
+                Primary
+                {priClassLevel.map((level) => {
+                  return (
+                    <div key={level}>
+                      <Field type='checkbox' name='classLevel' value={level} />
+                      {level}
+                    </div>
+                  );
+                })}
+                <br />
+                Secondary
+                {secClassLevel.map((level) => {
+                  return (
+                    <div key={level}>
+                      <Field type='checkbox' name='classLevel' value={level} />
+                      {level}
+                    </div>
+                  );
+                })}
+              </div>
+              {errors.classLevel && touched.classLevel ? (
+                <div>{errors.classLevel}</div>
+              ) : null}
+              <br />
+
+              <p>Subjects</p>
+              <div>
+                Primary
+                {priSubjects.map((subject) => {
+                  return (
+                    <div key={subject}>
+                      <Field type='checkbox' name='subjects' value={subject} />
+                      {subject}
+                    </div>
+                  );
+                })}
+                <br />
+                Secondary
+                {secSubjects.map((subject) => {
+                  return (
+                    <div key={subject}>
+                      <Field type='checkbox' name='subjects' value={subject} />
+                      {subject}
+                    </div>
+                  );
+                })}
+                <br />
+                Primary/Secondary
+                <br />
+                <Field type='checkbox' name='subjects' value='English' />
+                English
+              </div>
+              {errors.subjects && touched.subjects ? (
+                <div>{errors.subjects}</div>
+              ) : null}
+              {!matchingLevelSub && (
+                <p>Please select matching class levels and subjects.</p>
+              )}
+              <br />
+              <br />
+
+              <p>Education Background</p>
+              <Field
+                name='educationBackground'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.educationBackground}
+              />
+              {errors.educationBackground && touched.educationBackground ? (
+                <div>{errors.educationBackground}</div>
+              ) : null}
+              <p>Teaching Experience</p>
+              <Field
+                name='teachingExperience'
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.teachingExperience}
+              />
+              {errors.teachingExperience && touched.teachingExperience ? (
+                <div>{errors.teachingExperience}</div>
+              ) : null}
+              <br />
+              <button
+                type='submit'
+                disabled={
+                  !(
+                    Object.keys(errors).length === 0 &&
+                    Object.keys(touched).length ===
+                      Object.keys(initialValues).length
+                  )
+                }
+                style={{ backgroundColor: "lime" }}
+                onClick={handleCompleteEditing}>
+                Complete Editing
+              </button>
+              {/* {!isEmailUnique && <p>Email already in use!</p>} */}
+              {!isTutorProfileSetUp && (
+                <p>Tutor profile unable to be set up.</p>
+              )}
+              <CheckClassLevelAndSubject />
+            </Form>
+          )}
+        </Formik>
+      </>
+    );
+  }
 };
 
 export default EditTutorProfile;
