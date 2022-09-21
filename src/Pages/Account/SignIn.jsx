40c90fd3478a0cbed3336efd43a1ee925a2b36e9
tangjeraldine <typjera.1127@gmail.com>
2022-09-21 10:06:20 +0800

    Pulling changes.

--- src/Pages/Account/SignIn.jsx
+++ src/Pages/Account/SignIn.jsx
@@ -113,73 +113,71 @@ const SignIn = ({ setUser }) => {
                 <Form>
                   <main class='flex items-center justify-center '>
                     {" "}
-                    <form action='#' class='grid grid-cols-6 gap-6 mt-8'>
-                      <div class='col-span-6 sm:col-span-3'>
-                        <label
-                          for='Username'
-                          class='block text-sm font-medium text-gray-700'>
-                          Username
-                        </label>
-                        <Field
-                          id='username'
-                          name='username'
-                          type='text'
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.username}
-                          placeholder='Username'
-                          class='w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm'
-                        />
-                        {errors.username && touched.username ? (
-                          <div>{errors.username}</div>
-                        ) : null}
-                      </div>
-                      <div class='col-span-6 sm:col-span-3'>
-                        <label
-                          for='Password'
-                          class='block text-sm font-medium text-gray-700'>
-                          Password
-                        </label>
-                        <Field
-                          id='password'
-                          name='password'
-                          type='text'
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.password}
-                          placeholder='Password'
-                          class='w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm'
-                        />
-                        {errors.password && touched.password ? (
-                          <div>{errors.password}</div>
-                        ) : null}
-                      </div>
-                      <div class='col-span-6 col-start-3 col-end-5'>
-                        <button
-                          type='submit'
-                          disabled={
-                            !(
-                              Object.keys(errors).length === 0 &&
-                              Object.keys(touched).length !== 0
-                            )
-                          }
-                          class='inline-block px-12 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 flex justify-center'>
-                          Sign in
-                        </button>
-                      </div>
-                      <div class='col-span-6 col-start-2 col-end-5'>
-                        <p class='text-sm text-gray-700 float-right'>
-                          Don't have an account yet?
-                        </p>
-                        <button
-                          class='inline-block px-10 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 float-right'
-                          onClick={() => {
-                            navigate("/signup");
-                          }}>
-                          Create an account
-                        </button>
-                      </div>
-                    </form>
+                    <div class='col-span-6 sm:col-span-3'>
+                      <label
+                        for='Username'
+                        class='block text-sm font-medium text-gray-700'>
+                        Username
+                      </label>
+                      <Field
+                        id='username'
+                        name='username'
+                        type='text'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.username}
+                        placeholder='Username'
+                        class='w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm'
+                      />
+                      {errors.username && touched.username ? (
+                        <div>{errors.username}</div>
+                      ) : null}
+                    </div>
+                    <div class='col-span-6 sm:col-span-3'>
+                      <label
+                        for='Password'
+                        class='block text-sm font-medium text-gray-700'>
+                        Password
+                      </label>
+                      <Field
+                        id='password'
+                        name='password'
+                        type='text'
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                        placeholder='Password'
+                        class='w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm'
+                      />
+                      {errors.password && touched.password ? (
+                        <div>{errors.password}</div>
+                      ) : null}
+                    </div>
+                    <div class='col-span-6 col-start-3 col-end-5'>
+                      <button
+                        type='submit'
+                        disabled={
+                          !(
+                            Object.keys(errors).length === 0 &&
+                            Object.keys(touched).length !== 0
+                          )
+                        }
+                        class='inline-block px-12 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 flex justify-center'>
+                        Sign in
+                      </button>
+                    </div>
+                    <div class='col-span-6 col-start-2 col-end-5'>
+                      <p class='text-sm text-gray-700 float-right'>
+                        Don't have an account yet?
+                      </p>
+                      <button
+                        class='inline-block px-10 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 float-right'
+                        onClick={() => {
+                          navigate("/signup");
+                        }}>
+                        Create an account
+                      </button>
+                    </div>
                   </main>
 
                   {!signInSuccessful && <p>Sign in failed!</p>}
