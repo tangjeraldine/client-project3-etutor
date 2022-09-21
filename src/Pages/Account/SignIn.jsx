de4f5bf6855ff49e11e1dd40b078c4d028560a4c
Dewei <tan.dewei@hotmail.com>
2022-09-21 10:05:17 +0800

    added changes

--- src/Pages/Account/SignIn.jsx
+++ src/Pages/Account/SignIn.jsx
@@ -63,32 +63,32 @@ const SignIn = ({ setUser }) => {
   };
 
   return (
-    <section class='bg-yellow-300 h-screen'>
-      <div class='lg:grid lg:min-h-screen lg:grid-cols-12'>
-        <aside class='relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6'>
+    <section class="bg-yellow-300 h-screen">
+      <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside class="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
           <img
-            alt='Happy'
-            src='https://images.unsplash.com/photo-1597614645324-225ff8159d70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-            class='absolute inset-0 object-cover w-full h-full'
+            alt="Happy"
+            src="https://images.unsplash.com/photo-1597614645324-225ff8159d70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            class="absolute inset-0 object-cover w-full h-full"
           />
         </aside>
 
-        <main class='flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6'>
-          <div class='max-w-xl lg:max-w-3xl'>
-            <a class='block text-blue-600' href='/'>
-              <span class='sr-only'>Home</span>
+        <main class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6">
+          <div class="max-w-xl lg:max-w-3xl">
+            <a class="block text-blue-600" href="/">
+              <span class="sr-only">Home</span>
               <img
-                className='h-32'
-                src='https://cdn-icons-png.flaticon.com/512/3212/3212202.png'
-                alt='Welcome'
+                className="h-32"
+                src="https://cdn-icons-png.flaticon.com/512/3212/3212202.png"
+                alt="Welcome"
               />
             </a>
 
-            <h1 class='mt-6 text-2xl font-bold text-red-500 sm:text-3xl md:text-4xl'>
+            <h1 class="mt-6 text-2xl font-bold text-red-500 sm:text-3xl md:text-4xl">
               If you're striving for greatness, we're here for you.
             </h1>
 
-            <p class='mt-4 leading-relaxed text-rose-700'>
+            <p class="mt-4 leading-relaxed text-rose-700">
               eTutor is Singapore's leading hosting site for tutors and tutees
               to connect and manage their tuition sessions. Try now by signing
               up, setting up, or booking classes! Using this platform is
@@ -101,7 +101,8 @@ const SignIn = ({ setUser }) => {
                 password: "",
               }}
               validationSchema={signInValidation}
-              onSubmit={(values) => handleSignIn(values)}>
+              onSubmit={(values) => handleSignIn(values)}
+            >
               {({
                 handleChange,
                 handleBlur,
@@ -111,75 +112,77 @@ const SignIn = ({ setUser }) => {
                 initialValues,
               }) => (
                 <Form>
-                  <main class='flex items-center justify-center '>
+                  <main class="flex items-center justify-center ">
                     {" "}
-                    <form action='#' class='grid grid-cols-6 gap-6 mt-8'>
-                      <div class='col-span-6 sm:col-span-3'>
-                        <label
-                          for='Username'
-                          class='block text-sm font-medium text-gray-700'>
-                          Username
-                        </label>
-                        <Field
-                          id='username'
-                          name='username'
-                          type='text'
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.username}
-                          placeholder='Username'
-                          class='w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm'
-                        />
-                        {errors.username && touched.username ? (
-                          <div>{errors.username}</div>
-                        ) : null}
-                      </div>
-                      <div class='col-span-6 sm:col-span-3'>
-                        <label
-                          for='Password'
-                          class='block text-sm font-medium text-gray-700'>
-                          Password
-                        </label>
-                        <Field
-                          id='password'
-                          name='password'
-                          type='text'
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.password}
-                          placeholder='Password'
-                          class='w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm'
-                        />
-                        {errors.password && touched.password ? (
-                          <div>{errors.password}</div>
-                        ) : null}
-                      </div>
-                      <div class='col-span-6 col-start-3 col-end-5'>
-                        <button
-                          type='submit'
-                          disabled={
-                            !(
-                              Object.keys(errors).length === 0 &&
-                              Object.keys(touched).length !== 0
-                            )
-                          }
-                          class='inline-block px-12 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 flex justify-center'>
-                          Sign in
-                        </button>
-                      </div>
-                      <div class='col-span-6 col-start-2 col-end-5'>
-                        <p class='text-sm text-gray-700 float-right'>
-                          Don't have an account yet?
-                        </p>
-                        <button
-                          class='inline-block px-10 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 float-right'
-                          onClick={() => {
-                            navigate("/signup");
-                          }}>
-                          Create an account
-                        </button>
-                      </div>
-                    </form>
+                    <div class="col-span-6 sm:col-span-3">
+                      <label
+                        for="Username"
+                        class="block text-sm font-medium text-gray-700"
+                      >
+                        Username
+                      </label>
+                      <Field
+                        id="username"
+                        name="username"
+                        type="text"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.username}
+                        placeholder="Username"
+                        class="w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm"
+                      />
+                      {errors.username && touched.username ? (
+                        <div>{errors.username}</div>
+                      ) : null}
+                    </div>
+                    <div class="col-span-6 sm:col-span-3">
+                      <label
+                        for="Password"
+                        class="block text-sm font-medium text-gray-700"
+                      >
+                        Password
+                      </label>
+                      <Field
+                        id="password"
+                        name="password"
+                        type="text"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                        placeholder="Password"
+                        class="w-full mt-1 text-sm text-gray-700 bg-white border-gray-200 rounded-md shadow-sm"
+                      />
+                      {errors.password && touched.password ? (
+                        <div>{errors.password}</div>
+                      ) : null}
+                    </div>
+                    <div class="col-span-6 col-start-3 col-end-5">
+                      <button
+                        type="submit"
+                        disabled={
+                          !(
+                            Object.keys(errors).length === 0 &&
+                            Object.keys(touched).length !== 0
+                          )
+                        }
+                        class="inline-block px-12 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 flex justify-center"
+                      >
+                        Sign in
+                      </button>
+                    </div>
+                    <div class="col-span-6 col-start-2 col-end-5">
+                      <p class="text-sm text-gray-700 float-right">
+                        Don't have an account yet?
+                      </p>
+                      <button
+                        class="inline-block px-10 py-2 text-sm font-medium text-white transition bg-red-500 border border-black-600 rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 float-right"
+                        onClick={() => {
+                          navigate("/signup");
+                        }}
+                      >
+                        Create an account
+                      </button>
+                    </div>
                   </main>
 
                   {!signInSuccessful && <p>Sign in failed!</p>}
--- src/Pages/Tutor/MyTutees.jsx
+++ src/Pages/Tutor/MyTutees.jsx
@@ -1,6 +1,7 @@
 import urlcat from "urlcat";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import TuteeModal from "../../components/TuteeModal";
 
 const SERVER = import.meta.env.VITE_SERVER;
 const url = urlcat(SERVER, "/tutee");
@@ -8,6 +9,15 @@ const url = urlcat(SERVER, "/tutee");
 const MyTutees = ({ user }) => {
   const [myTutee, setMyTutee] = useState([]);
   const [pendingTutee, setPendingTutee] = useState([]);
+  const [tutorDetails, setTutorDetails] = useState([]);
+  const [tuteeDetails, setTuteeDetails] = useState([]);
+
+  // use the tutor's user_id to find tutees
+
+  // pending tutors
+  // pressing accept, -> delete tutee from pendingtutors and add tutee to myTutors
+  // pressing reject -> delete tutee from pending tutors only
+  // when tutors
 
   useEffect(() => {
     //get tutor details to see their subjects, class levels, and class types of specific tutor
@@ -25,23 +35,58 @@ const MyTutees = ({ user }) => {
       });
   }, []);
 
+  useEffect(() => {
+    //access tutees database and find all tutees of this tutor
+    const urlTuteeDetails = urlcat(
+      SERVER,
+      `/tutee/myTutees/${tutorDetails?._id}`
+    );
+    axios
+      .get(urlTuteeDetails)
+      .then(({ data }) => {
+        console.log(data);
+        setTuteeDetails(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [tutorDetails]);
+
   return (
     <>
       <div>
         <h1 style={{ fontSize: "30px" }}>my tutees</h1>
-        {/* {myTutee.length === 0 ? (
-          <div>You have no tutee</div>
+        {tuteeDetails.length === 0 ? (
+          <div>You have no tutees</div>
         ) : (
-          myTutee.map((tutee) => <p key={tutee._id}>{tutee.fullName}</p>)
-        )} */}
-        <p>tutor #1</p>
-        <p>tutor #2</p>
+          tuteeDetails?.map((tutee) => {
+            if (tutee.myTutors.includes(tutorDetails._id)) {
+              return <p key={tutee._id}>{tutee.fullName}</p>;
+            }
+          })
+        )}
       </div>
       <br />
       <div>
         <h1 style={{ fontSize: "30px" }}>pending tutees</h1>
-        <p>tutor #3</p>
-        <p>tutor #4</p>
+        {tuteeDetails.length === 0 ? (
+          <div>You have no pending tutees</div>
+        ) : (
+          tuteeDetails?.map((tutee, index) => {
+            if (tutee.pendingTutors.includes(tutorDetails._id)) {
+              return (
+                <div key={index} onClick={() => console.log("")}>
+                  <p key={tutee._id}>{tutee.fullName}</p>
+                  {/* <button style={{ backgroundColor: "lime" }}>accept</button>
+                  <button style={{ backgroundColor: "red" }}>reject</button> */}
+                </div>
+              );
+            }
+          })
+        )}
+        <div>
+          <TuteeModal />
+        </div>
       </div>
     </>
   );
