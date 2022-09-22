397769ec13f64d84564c8fed1f335508761e8c7c
maryam adn <maryamahmaddahlan@gmail.com>
2022-09-22 12:27:01 +0800

    successfully set up my classes tutee

--- src/Pages/Tutee/MyClassesTutee.jsx
+++ src/Pages/Tutee/MyClassesTutee.jsx
@@ -10,15 +10,15 @@ import { format, parseISO } from "date-fns";
 const SERVER = import.meta.env.VITE_SERVER;
 
 const MyClassesTutee = ({ user }) => {
-
   const [classes, setClasses] = useState([]);
   const [availableClasses, setAvailableClasses] = useState([]);
   const [tuteeDetails, setTuteeDetails] = useState({});
   const [renderClasses, setRenderClasses] = useState(true);
-  const [bookedClasses, setBookedClasses] = useState(false)
-  const [anyAvailableClasses, setAnyAvailableClasses] = useState(false)
+  const [bookedClasses, setBookedClasses] = useState(false);
+  const [anyAvailableClasses, setAnyAvailableClasses] = useState(true);
   const [loadClassesSuccessful, setLoadClassesSuccessful] = useState(true);
-  const [loadAvailableClassesSuccessful, setLoadAvailableClassesSuccessful] = useState(true);
+  const [loadAvailableClassesSuccessful, setLoadAvailableClassesSuccessful] =
+    useState(true);
   const [addBookingSuccessful, setAddBookingSuccessful] = useState(true);
   const [removeBookingSuccessful, setRemoveBookingSuccessful] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
@@ -26,10 +26,7 @@ const MyClassesTutee = ({ user }) => {
 
   useEffect(() => {
     //fetch current tutee's data
-    const urlTuteeDetails = urlcat(
-      SERVER,
-      `/tutee/tuteedetails/${user._id}`
-    );
+    const urlTuteeDetails = urlcat(SERVER, `/tutee/tuteedetails/${user._id}`);
     axios
       .get(urlTuteeDetails)
       .then(({ data }) => {
@@ -43,7 +40,6 @@ const MyClassesTutee = ({ user }) => {
       });
   }, []);
 
-  
   useEffect(() => {
     if (tuteeDetails._id !== undefined) {
       const urlGetClasses = urlcat(
@@ -54,11 +50,11 @@ const MyClassesTutee = ({ user }) => {
         .get(urlGetClasses)
         .then(({ data }) => {
           if (data.length === 0) {
-            setBookedClasses(false)
+            setBookedClasses(false);
           } else {
-            console.log(data)
+            console.log(data);
             setClasses(data);
-            setBookedClasses(true)
+            setBookedClasses(true);
           }
         })
         .catch((error) => {
@@ -66,31 +62,35 @@ const MyClassesTutee = ({ user }) => {
             setLoadClassesSuccessful(false);
           }
         });
-        
-        const tutorIdArray = []
-        tuteeDetails.myTutors.map(tutor => tutorIdArray.push(tutor._id))
-        const urlGetAvailableClasses = urlcat(
-          SERVER,
-          `/class/get-available-classes/${tuteeDetails._id}/${tutorIdArray.join(' ')}`
-        );
-        axios
-          .get(urlGetAvailableClasses)//only get avail classes that match tutee's level and subject..
-          .then(({ data }) => {
-            if (data.length === 0) {
-              setAnyAvailableClasses(false)
-            } else {
-              console.log(data)
-              setAvailableClasses(data);
-              setAnyAvailableClasses(true)
-            }
-          })
-          .catch((error) => {
-            if (error.response.data.error === "Unable to load classes.") {
-              setLoadAvailableClassesSuccessful(false);
-            }
-          });
-    }
 
+      const tutorIdArray = [];
+      tuteeDetails.myTutors.map((tutor) => tutorIdArray.push(tutor._id));
+      const tuteeSubjectArray = [];
+      tuteeDetails.subjects.map((subject) => tuteeSubjectArray.push(subject));
+      const urlGetAvailableClasses = urlcat(
+        SERVER,
+        `/class/get-available-classes/${tuteeDetails._id}/${tutorIdArray.join(
+          " "
+        )}/${tuteeSubjectArray.join(" ")}/${tuteeDetails.currentLevel}`
+      );
+      axios
+        .get(urlGetAvailableClasses) //only get avail classes that match tutee's level and subject..
+        .then(({ data }) => {
+          if (data.length === 0) {
+            setAnyAvailableClasses(false);
+            setAvailableClasses(data);
+          } else {
+            console.log(data);
+            setAvailableClasses(data);
+            setAnyAvailableClasses(true);
+          }
+        })
+        .catch((error) => {
+          if (error.response.data.error === "Unable to load classes.") {
+            setLoadAvailableClassesSuccessful(false);
+          }
+        });
+    }
   }, [renderClasses, tuteeDetails]);
 
   //find and update the class's bookedby and group size
@@ -103,16 +103,14 @@ const MyClassesTutee = ({ user }) => {
       .put(urlAddBooking)
       .then(({ data }) => {
         console.log(data);
-          setRenderClasses(!renderClasses);
+        setRenderClasses(!renderClasses);
       })
       .catch((error) => {
-        if (
-          error.response.data.error === "Class not found."
-        ) {
+        if (error.response.data.error === "Class not found.") {
           setAddBookingSuccessful(false);
         }
       });
-  }
+  };
 
   const handleRemoveBooking = (id) => {
     const urlRemoveBooking = urlcat(
@@ -123,12 +121,10 @@ const MyClassesTutee = ({ user }) => {
       .put(urlRemoveBooking)
       .then(({ data }) => {
         console.log(data);
-          setRenderClasses(!renderClasses);
+        setRenderClasses(!renderClasses);
       })
       .catch((error) => {
-        if (
-          error.response.data.error === "Class not found."
-        ) {
+        if (error.response.data.error === "Class not found.") {
           setRemoveBookingSuccessful(false);
         }
       });
@@ -139,7 +135,6 @@ const MyClassesTutee = ({ user }) => {
     setWhatToOpen(index);
   };
 
-
   return (
     <>
       <h1 style={{ fontSize: "50px" }}>My Classes</h1>
@@ -147,7 +142,7 @@ const MyClassesTutee = ({ user }) => {
         {classes.map((eachClass, index) => {
           // const tutees = [];
           // eachClass.bookedBy.map((tutee) => tutees.push(tutee.fullName));
-          const filledSlots = eachClass.bookedBy.length
+          const filledSlots = eachClass.bookedBy.length;
           const timeDay = format(
             parseISO(eachClass.timeDay),
             "EEE, dd/MM/yyyy, hh:mm aaaa"
@@ -155,21 +150,26 @@ const MyClassesTutee = ({ user }) => {
           return (
             <div key={index}>
               <p>Class Title: {eachClass.classTitle}</p>
+              <p>Tutor: {eachClass.tutor.fullName}</p>
               <p>Date, Time: {timeDay}</p>
               <p>Class Type: {eachClass.classType}</p>
               <p>Subject: {eachClass.subject}</p>
               <p>Class Level: {eachClass.classLevel}</p>
               {/* <p>Tutees: {tutees.join(", ") || "none"}</p> */}
-              <p>Group Size: {filledSlots}/{eachClass.groupSize}</p>
+              <p>
+                Group Size: {filledSlots}/{eachClass.groupSize}
+              </p>
               <button
                 style={{ backgroundColor: "lime" }}
-                onClick={() => handleRemoveBooking(eachClass._id)}>
+                onClick={() => handleRemoveBooking(eachClass._id)}
+              >
                 remove class
               </button>
 
               <button
                 style={{ backgroundColor: "lime" }}
-                onClick={() => handleModal(index)}>
+                onClick={() => handleModal(index)}
+              >
                 details
               </button>
             </div>
@@ -178,16 +178,18 @@ const MyClassesTutee = ({ user }) => {
         {!bookedClasses && <p>No classes booked yet.</p>}
         {!loadClassesSuccessful && <p>Unable to load classes.</p>}
         {!removeBookingSuccessful && <p>Unable to remove booking.</p>}
-        
       </div>
 
       <h1 style={{ fontSize: "50px" }}>Available Classes</h1>
       <div>
         {availableClasses.map((eachClass, index) => {
-          const filledSlots = eachClass.bookedBy.length
-          if (filledSlots === eachClass.groupSize) {
-            return
-          }
+          const filledSlots = eachClass.bookedBy.length;
+          // if (filledSlots === eachClass.groupSize
+          //   // || !(tuteeDetails.subjects.includes(eachClass.subject)) || tuteeDetails.currentLevel !== eachClass.classLevel
+          //   ) {
+          //   //still need to set count
+          //   return
+          // }
           const timeDay = format(
             parseISO(eachClass.timeDay),
             "EEE, dd/MM/yyyy, hh:mm aaaa"
@@ -196,28 +198,35 @@ const MyClassesTutee = ({ user }) => {
           return (
             <div key={index}>
               <p>Class Title: {eachClass.classTitle}</p>
+              <p>Tutor: {eachClass.tutor.fullName}</p>
               <p>Date, Time: {timeDay}</p>
               <p>Class Type: {eachClass.classType}</p>
               <p>Subject: {eachClass.subject}</p>
               <p>Class Level: {eachClass.classLevel}</p>
               {/* <p>Tutees: {tutees.join(", ") || "none"}</p> do we wanna show tutees? so idk lol tutees can see if their friends booked or not or smtg idk lol*/}
-              <p>Group Size: {filledSlots}/{eachClass.groupSize}</p>
+              <p>
+                Group Size: {filledSlots}/{eachClass.groupSize}
+              </p>
               <button
                 style={{ backgroundColor: "lime" }}
-                onClick={() => handleAddBooking(eachClass._id)}>
+                onClick={() => handleAddBooking(eachClass._id)}
+              >
                 book class
               </button>
 
               <button
                 style={{ backgroundColor: "lime" }}
-                onClick={() => handleModal(index)}>
+                onClick={() => handleModal(index)}
+              >
                 details
               </button>
             </div>
           );
         })}
         {!anyAvailableClasses && <p>No available classes yet.</p>}
-        {!loadAvailableClassesSuccessful && <p>Unable to load available classes.</p>}
+        {!loadAvailableClassesSuccessful && (
+          <p>Unable to load available classes.</p>
+        )}
         {!addBookingSuccessful && <p>Unable to add booking.</p>}
       </div>
 
@@ -227,7 +236,6 @@ const MyClassesTutee = ({ user }) => {
         eachClass={classes[whatToOpen]}
         tuteeDetails={tuteeDetails}
       /> */}
-
     </>
   );
 };
